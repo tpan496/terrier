f0deb41a3f9e7fa9bf001d5ccf6d695708d297a3
GustavoAngulo <gangulo@andrew.cmu.edu>
2019-10-02 12:09:30 -0400

    Fix provider and add single threaded test

--- src/include/storage/recovery/replication_log_provider.h
+++ src/include/storage/recovery/replication_log_provider.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <string>
+#include "network/network_io_utils.h"
 #include "storage/recovery/abstract_log_provider.h"
 #include "storage/write_ahead_log/log_io.h"
-#include "network/network_io_utils.h"
 
 namespace terrier::storage {
 
@@ -13,17 +13,25 @@ namespace terrier::storage {
  */
 class ReplicationLogProvider : public AbstractLogProvider {
  public:
-  ReplicationLogProvider() : replication_active_(true) {}
+  ReplicationLogProvider(std::chrono::seconds replication_timeout)
+      : replication_active_(true), replication_timeout_(replication_timeout) {}
 
+  /**
+   * Notifies the log provider that replication is ending
+   */
   void EndReplication() {
     std::unique_lock<std::mutex> lock(replication_latch_);
     replication_active_ = false;
     replication_cv_.notify_one();
   }
 
+  /**
+   * Hands a buffer of log records to the replication provider
+   * @param buffer buffer of log records
+   */
   void HandBufferToReplication(std::unique_ptr<network::ReadBuffer> buffer) {
     std::unique_lock<std::mutex> lock(replication_latch_);
-    arrived_buffer_queue_.emplace(buffer);
+    arrived_buffer_queue_.emplace(std::move(buffer));
     replication_cv_.notify_one();
   }
 
@@ -32,13 +40,14 @@ class ReplicationLogProvider : public AbstractLogProvider {
   bool replication_active_;
 
   // TODO(Gus): Put in settings manager
-  std::chrono::minutes replication_timeout_{20};
+  std::chrono::seconds replication_timeout_;
 
   // Current buffer to read logs from
   std::unique_ptr<network::ReadBuffer> curr_buffer_ = nullptr;
 
   // Buffers that have arrived in packets from the master node
-  // TODO(Gus): For now, these buffers will most likely be allocated on demand by the network layer. We could consider having a buffer pool if allocation becomes a bottleneck
+  // TODO(Gus): For now, these buffers will most likely be allocated on demand by the network layer. We could consider
+  // having a buffer pool if allocation becomes a bottleneck
   std::queue<std::unique_ptr<network::ReadBuffer>> arrived_buffer_queue_;
 
   // Synchronisation primitives to synchronise arrival of packets and process termination
@@ -46,17 +55,22 @@ class ReplicationLogProvider : public AbstractLogProvider {
   std::condition_variable replication_cv_;
 
   /**
-   * @return true if log file contains more records, false otherwise. Can be blocking if waiting for message from master, or master has timed out
+   * @return true if log file contains more records, false otherwise. Can be blocking if waiting for message from
+   * master, or master has timed out
    */
   bool HasMoreRecords() override {
     std::unique_lock<std::mutex> lock(replication_latch_);
     // We wake up from CV if:
     //  1. We time out
     //  2. Someone ends replication
-    //  3. A new packet has arrived
-    auto timeout = replication_cv_.wait_for(lock, replication_timeout_,
-        [&] { return !replication_active_ || !arrived_buffer_queue_.empty(); });
-    return !timeout && replication_active_ && !arrived_buffer_queue_.empty();
+    //  3. We have a current buffer and it has more bytes availible
+    //  4. A new packet has arrived
+    bool predicate = replication_cv_.wait_for(lock, replication_timeout_, [&] {
+      return !replication_active_ || (curr_buffer_ != nullptr && curr_buffer_->HasMore()) ||
+             !arrived_buffer_queue_.empty();
+    });
+    // If we time out, predicate == false
+    return predicate && replication_active_;
   }
 
   /**
@@ -68,12 +82,13 @@ class ReplicationLogProvider : public AbstractLogProvider {
   bool Read(void *dest, uint32_t size) override {
     if (curr_buffer_ == nullptr || !curr_buffer_->HasMore()) {
       std::unique_lock<std::mutex> lock(replication_latch_);
-      auto timeout = replication_cv_.wait_for(lock, replication_timeout_,
-          [&] { return !replication_active_ || !arrived_buffer_queue_.empty(); });
+      bool predicate = replication_cv_.wait_for(lock, replication_timeout_,
+                                                [&] { return !replication_active_ || !arrived_buffer_queue_.empty(); });
       // If we timeout or replication is shut down, return false
-      if (!timeout || !replication_active_) return false;
+      if (!predicate || !replication_active_) return false;
 
-      TERRIER_ASSERT(!arrived_buffer_queue_.empty(), "If we did not shut down or timeout, CV should only wake up when a new buffer arrives");
+      TERRIER_ASSERT(!arrived_buffer_queue_.empty(),
+                     "If we did not shut down or timeout, CV should only wake up when a new buffer arrives");
       curr_buffer_ = std::move(arrived_buffer_queue_.front());
       arrived_buffer_queue_.pop();
     }
@@ -84,7 +99,7 @@ class ReplicationLogProvider : public AbstractLogProvider {
 
     // If we didn't read all that we needed to, call Read again to read the rest of the data
     // We cast dest to char* to increase pointer by the size we read.
-    return (readable_size < size) ? Read(static_cast<char*>(dest) + readable_size, size - readable_size) : true;
+    return (readable_size < size) ? Read(static_cast<char *>(dest) + readable_size, size - readable_size) : true;
   }
 };
 
