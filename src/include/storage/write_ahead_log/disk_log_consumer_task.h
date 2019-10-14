c32e154a6899b654f47a76e836b5aea13b222968
GustavoAngulo <gangulo@andrew.cmu.edu>
2019-10-13 23:21:19 -0400

    Add base log consumer task class

--- src/include/storage/write_ahead_log/disk_log_consumer_task.h
+++ src/include/storage/write_ahead_log/disk_log_consumer_task.h
@@ -2,10 +2,7 @@
 
 #include <utility>
 #include <vector>
-#include "common/container/concurrent_blocking_queue.h"
-#include "common/dedicated_thread_registry.h"
-#include "storage/storage_defs.h"
-#include "storage/write_ahead_log/log_io.h"
+#include "storage/write_ahead_log/log_consumer_task.h"
 
 namespace terrier::storage {
 
@@ -13,27 +10,24 @@ namespace terrier::storage {
  * A DiskLogConsumerTask is responsible for writing serialized log records out to disk by processing buffers in the log
  * manager's filled buffer queue
  */
-class DiskLogConsumerTask : public common::DedicatedThreadTask {
+class DiskLogConsumerTask final : public LogConsumerTask {
  public:
   /**
    * Constructs a new DiskLogConsumerTask
    * @param log_file_path path to file to write logs to
    * @param persist_interval Interval time for when to persist log file
    * @param persist_threshold threshold of data written since the last persist to trigger another persist
-   * @param empty_buffer_queue pointer to queue to push empty buffers to
-   * @param filled_buffer_queue pointer to queue to pop filled buffers from
+
    */
   explicit DiskLogConsumerTask(const char *log_file_path, const std::chrono::milliseconds persist_interval,
                                uint64_t persist_threshold,
                                common::ConcurrentBlockingQueue<BufferedLogWriter *> *empty_buffer_queue,
                                common::ConcurrentQueue<storage::SerializedLogs> *filled_buffer_queue)
-      : run_task_(false),
+      : LogConsumerTask(empty_buffer_queue, filled_buffer_queue),
         out_(PosixIoWrappers::Open(log_file_path, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR)),
         persist_interval_(persist_interval),
         persist_threshold_(persist_threshold),
-        current_data_written_(0),
-        empty_buffer_queue_(empty_buffer_queue),
-        filled_buffer_queue_(filled_buffer_queue) {}
+        current_data_written_(0) {}
 
   /**
    * Runs main disk log writer loop. Called by thread registry upon initialization of thread
@@ -46,9 +40,6 @@ class DiskLogConsumerTask : public common::DedicatedThreadTask {
   void Terminate() override;
 
  private:
-  friend class LogManager;
-  // Flag to signal task to run or stop
-  bool run_task_;
   // File descriptor for log file
   int out_;
   // Stores callbacks for commit records written to disk but not yet persisted
@@ -60,11 +51,6 @@ class DiskLogConsumerTask : public common::DedicatedThreadTask {
   uint64_t persist_threshold_;
   // Amount of data written since last persist
   uint64_t current_data_written_;
-
-  // The queue containing empty buffers. Task will enqueue a buffer into this queue when it has flushed its logs
-  common::ConcurrentBlockingQueue<BufferedLogWriter *> *empty_buffer_queue_;
-  // The queue containing filled buffers. Task should dequeue filled buffers from this queue to flush
-  common::ConcurrentQueue<SerializedLogs> *filled_buffer_queue_;
 
   // Flag used by the serializer thread to signal the disk log consumer task thread to persist the data on disk
   volatile bool do_persist_;
