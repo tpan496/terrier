6c5e3540cfae607e9e0b10ec24564cc08b1d128d
tpan496 <tianleip@andrew.cmu.edu>
2021-07-06 13:57:08 -0400

    Update recovery_manager.cpp

--- src/storage/recovery/recovery_manager.cpp
+++ src/storage/recovery/recovery_manager.cpp
@@ -1181,7 +1181,7 @@ void RecoveryManager::InsertRedoRecordToInsertTranslator(transaction::Transactio
                                                          common::ManagedPointer<storage::SqlTable> sql_table,
                                                          storage::RedoRecord *redo_record,
                                                          std::vector<byte *> varlen_contents) {
-  auto t0 = std::chrono::high_resolution_clock::now();
+  //auto t0 = std::chrono::high_resolution_clock::now();
   //auto t1 = std::chrono::high_resolution_clock::now();
   std::unique_ptr<catalog::CatalogAccessor> accessor =
       catalog_->GetAccessor(common::ManagedPointer(txn), redo_record->GetDatabaseOid(), DISABLED);
@@ -1336,11 +1336,11 @@ void RecoveryManager::InsertRedoRecordToInsertTranslator(transaction::Transactio
     exec_ctx->SetParams(common::ManagedPointer<const std::vector<parser::ConstantValueExpression>>(&params));
   //}
 
-  auto t2 = std::chrono::high_resolution_clock::now();
+  //auto t2 = std::chrono::high_resolution_clock::now();
 
   exec_queries_[query_identifier]->Run(common::ManagedPointer(exec_ctx), execution::vm::ExecutionMode::Compiled);
-  auto t3 = std::chrono::high_resolution_clock::now();
-  EXECUTION_LOG_ERROR("Prep: {}, Run: {}", std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t0).count(), std::chrono::duration_cast<std::chrono::nanoseconds>(t3 - t2).count());
+  //auto t3 = std::chrono::high_resolution_clock::now();
+  //EXECUTION_LOG_ERROR("Prep: {}, Run: {}", std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t0).count(), std::chrono::duration_cast<std::chrono::nanoseconds>(t3 - t2).count());
 
   // Update tuple slots.
   auto new_tuple_slot = *exec_ctx->GetTupleSlot();
